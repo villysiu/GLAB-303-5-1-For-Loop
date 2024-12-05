287ec54aaebb7955db2ea3ce0e2e8189dc584c63
Villy Siu <villysiu@gmail.com>
2024-12-05 14:37:51 -0800

    added

--- src/Loopdemoone.java
+++ src/Loopdemoone.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Loopdemoone {
+    public static void main(String[] args) {
+        int n = 5;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            System.out.println("Java is fun");
+        }
+    }
+
 }
--- src/SumOfNaturalNumbers.java
+++ src/SumOfNaturalNumbers.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        int n = 1000;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            // body inside for loop
+            sum += i;     // sum = sum + i
+        }
+        System.out.println("Sum = " + sum);
+    }
+
 }
